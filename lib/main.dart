7171fd74f2ee9e464fd31890530596f968b634c8
Cyrillus Rudi Soru <abdullahrudisoru@gmail.com>
2025-02-09 14:54:37 +0700

    Question 014

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:basic_flutter_exercises/questions/question_010.dart';
 import 'package:basic_flutter_exercises/questions/question_011.dart';
 import 'package:basic_flutter_exercises/questions/question_012.dart';
 import 'package:basic_flutter_exercises/questions/question_013.dart';
+import 'package:basic_flutter_exercises/questions/question_014.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
     // return Question010();
     // return Question011();
     // return Question012();
-    return Question013();
+    // return Question013();
+    return Question014();
   }
 }
